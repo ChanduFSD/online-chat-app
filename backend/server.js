0781b3079a76e19465d4e4e7aaf2f15130d19530
chandu <chandus1510@gmail.com>
2024-03-07 17:17:10 +0530

    auth route setup

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,6 @@
 import express from "express";
 import dotenv from "dotenv" ;
+import authRoutes from "./routes/auth.routes.js"
 
 const app = express();
 dotenv.config();
@@ -7,8 +8,10 @@ dotenv.config();
 const PORT = process.env.PORT || 5000;
 
 app.get("/",(req,res)=>{
-    res.send("serever is ready machii yen henge hediya")
+    res.send("serever is ready ")
 })
+
+app.use("/api/auth",authRoutes)
 
 app.listen(PORT,()=>{
     console.log(`server is running in 6000 https://localhost:${PORT}`)
