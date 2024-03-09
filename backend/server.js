090be2950dcd4a23d2fe2d8afd4ab0f0400a655c
chandu <chandus1510@gmail.com>
2024-03-09 23:54:24 +0530

    #

--- backend/server.js
+++ backend/server.js
@@ -1,3 +1,4 @@
+import path from "path";
 import express from "express";
 import dotenv from "dotenv" ;
 import cookieParser from "cookie-parser";
@@ -12,7 +13,7 @@ import { app, server } from "./socket/socket.js";
 
 const PORT = process.env.PORT || 5000;
 
-
+const __dirname = path.resolve();
 
 
 dotenv.config();
@@ -23,6 +24,13 @@ app.use(cookieParser());
 app.use("/api/auth",authRoutes);
 app.use("/api/messages",messageRoutes);
 app.use("/api/users",userRoutes);
+
+
+app.use(express.static(path.join(__dirname, "/frontenda/dist")));
+
+app.get("*", (req, res) => {
+	res.sendFile(path.join(__dirname, "frontenda", "dist", "index.html"));
+});
 
 // app.get("/",(req,res)=>{
 //     res.send("serever is ready ")
