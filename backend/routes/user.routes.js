09c34e9113fa23f412fb617a6056c77bee137852
chandu <chandus1510@gmail.com>
2024-03-13 12:35:51 +0530

    #

--- backend/routes/user.routes.js
+++ backend/routes/user.routes.js
@@ -1,6 +1,6 @@
 import express from "express";
 
-import protectRoute from "../middelware/protectRoute.js";
+import protectRoute from "../middleware/protectRoute.js";
 import { getUsersForSidebar } from "../controllers/user.controller.js";
 
 const router = express.Router();
